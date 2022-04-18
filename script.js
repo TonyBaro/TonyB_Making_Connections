e420b790dd47a091d2715ba99e43797da7e2f5b2
TonyB <tonybaromagic@gmail.com>
2022-04-18 18:32:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("page loaded")
+
+function nameChange(){
+    document.querySelector(".userlog h2").innerHTML = "Johny Doerson"
+    document.getElementById("userpic").src = "Assets/images/todd-s.jpg"
+}
